e0ebd115bf72c63b90aa734e7ae7667903f52610
luibasantes <luibasantes@hotmail.com>
2019-08-19 01:07:52 -0500

    Implementacion de la funcionalidad y diseño de Consultar por Sector

--- MISIVSWebApp/Controllers/HomeController.cs
+++ MISIVSWebApp/Controllers/HomeController.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Data.Entity;
+
 using System.Web;
 using System.Web.Mvc;
 using MISIVSWebApp.Models;
+using LinqGrouping.Models;
+using System.Net;
 
 namespace MISIVSWebApp.Controllers
 {
@@ -25,8 +29,20 @@ namespace MISIVSWebApp.Controllers
 
         public ActionResult ConsultSectors()
         {
-            ViewBag.Message = "ConsultSectors";
 
+            List<int> list = new List<int>();
+            list.Add(2);
+            list.Add(3);
+            list.Add(7);
+
+
+
+            db.Configuration.ProxyCreationEnabled = false;
+            var booksGrouped = from b in db.Vivienda
+                               group b by b.sector into g
+                               select new Group<string, Vivienda> { Key = g.Key, Values = g };
+
+            ViewBag.sectores = booksGrouped.ToList();
             return View();
         }
 
@@ -47,12 +63,42 @@ namespace MISIVSWebApp.Controllers
 
         public ActionResult GetByInspector()
         {
-
-            db.Configuration.ProxyCreationEnabled = false;
+            /*db.Configuration.ProxyCreationEnabled = false;
             var fichaList = db.Ficha.ToList();
+            return Json(new { data = fichaList }, JsonRequestBehavior.AllowGet);*/
+            db.Configuration.ProxyCreationEnabled = false;
+            var fichaList = db.Ficha.Include(f => f.Vivienda).Where(i => i.activo==true);
+
+            foreach (Ficha fic in fichaList)
+            {
+                fic.Vivienda.Ficha = null;
+            }
+
             return Json(new { data = fichaList }, JsonRequestBehavior.AllowGet);
         }
 
 
+        public ActionResult ViviendasBySector(String id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            db.Configuration.ProxyCreationEnabled = false;
+            var viviendasList = db.Vivienda.Where(x => x.sector == id);
+            foreach (Vivienda viv in viviendasList)
+            {
+                viv.Ficha = null;
+            }
+
+
+
+
+            
+            return Json(new { data = viviendasList }, JsonRequestBehavior.AllowGet);
+        }
+
     }
 }
+
+
---
+++ MISIVSWebApp/Group.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LinqGrouping.Models
+{
+    //Clase para manejar groupby
+    public class Group<K, T>
+    {
+        public K Key;
+        public IEnumerable<T> Values;
+    }
+}
